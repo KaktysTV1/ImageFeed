11f2599809ec8d09f7730e11d573fbab4dd17ee4
KaktysTV1 <wotmaster187@iclod.com>
2023-09-01 23:46:41 +0300

    Выполнено задание восьмого спринта

--- ImageFeed/imagesList/ImagesListViewController.swift
+++ ImageFeed/imagesList/ImagesListViewController.swift
@@ -11,6 +11,8 @@ class ImagesListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
+    private let photosName: [String] = Array(0..<20).map{"\($0)"}
+    
     private lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.dateStyle = .long
@@ -20,6 +22,8 @@ class ImagesListViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
        
         tableView.dataSource = self
     }
@@ -27,7 +31,7 @@ class ImagesListViewController: UIViewController {
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -44,9 +48,32 @@ extension ImagesListViewController: UITableViewDataSource {
 }
 
 extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) { }
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        }
+        
+        cell.cellImage.image = image
+        cell.dataLable.text = dateFormatter.string(from: Date())
+        
+        let isLike = indexPath.row % 2 == 0
+        let likeImage = isLike ? UIImage(named: "NoActiveLikeButton") : UIImage(named: "ActiveLikeButton")
+        cell.likeButton.setImage(likeImage, for: .normal)
+    }
 }
 
 extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return 0
+        }
+        let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+        let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+        let imageWidth = image.size.width
+        let scale = imageViewWidth / imageWidth
+        let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+        return cellHeight
+    }
 }
