11479420fd38d100ea275b203b660f557a0adcfa
KaktysTV1 <wotmaster187@iclod.com>
2023-09-07 22:00:28 +0300

    Сверстан экран профиля

---
+++ ImageFeed/ImagesList/Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Чупрыненко on 07.09.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
