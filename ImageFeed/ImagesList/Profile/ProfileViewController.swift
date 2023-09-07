670d80b1e5529e6a05c38cecb4376adaea1eab71
KaktysTV1 <wotmaster187@iclod.com>
2023-09-07 23:56:10 +0300

    Добавлены аутлеты

--- ImageFeed/ImagesList/Profile/ProfileViewController.swift
+++ ImageFeed/ImagesList/Profile/ProfileViewController.swift
@@ -8,5 +8,18 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
+    @IBOutlet weak var avatarImage: UIImageView!
+    
+    @IBOutlet weak var descriptionLable: UILabel!
+    
+    @IBOutlet weak var loginLableName: UILabel!
+    
+    @IBOutlet weak var nameLable: UILabel!
+    
+    @IBOutlet weak var logoutButton: UIButton!
+    
+    @IBAction func didTapLogoutButton() {
+    }
+    
     
 }
