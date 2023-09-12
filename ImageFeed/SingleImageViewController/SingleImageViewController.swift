1c1d7a800b3418b9997fec42208a951d6c7c15ee
KaktysTV1 <wotmaster187@iclod.com>
2023-09-12 14:32:36 +0300

    Добавлен экран с увеличенным изображением

---
+++ ImageFeed/SingleImageViewController/SingleImageViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Андрей Чупрыненко on 12.09.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+}
