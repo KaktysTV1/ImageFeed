16919620b1a327648baad5326244722fe0cb0ff9
KaktysTV1 <wotmaster187@iclod.com>
2023-10-29 20:27:32 +0300

    Иницилизация ветки

---
+++ ImagesListServiceTests/ImagesListServiceTests.swift
@@ -0,0 +1,14 @@
+//
+//  ImagesListServiceTests.swift
+//  ImagesListServiceTests
+//
+//  Created by Андрей Чупрыненко on 29.10.2023.
+//
+
+@testable import ImageFeed
+import XCTest
+
+final class ImagesListServiceTests: XCTestCase {
+    func testExample() {
+    }
+}
