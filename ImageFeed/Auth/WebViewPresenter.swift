c0afb49f9a7aba4f75931f8ca9c1783105a309c1
KaktysTV1 <wotmaster187@iclod.com>
2023-11-05 18:53:39 +0300

    Создан presenter для webview. Вынесены ответственности

---
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -0,0 +1,62 @@
+//
+//  WebViewPresenter.swift
+//  ImageFeed
+//
+//  Created by Андрей Чупрыненко on 05.11.2023.
+//
+
+import Foundation
+
+fileprivate let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+
+public protocol WebViewPresenterProtocol {
+    var view: WebViewViewControllerProtocol? {get set}
+    func viewDidLoad()
+    func didUpdateProgressValue(_ newValue: Double)
+    func code(from url: URL) -> String?
+}
+
+final class WebViewPresenter: WebViewPresenterProtocol {
+    func code(from url: URL) -> String? {
+        if
+            let urlComponents = URLComponents(string: url.absoluteString),
+            urlComponents.path == "/oauth/authorize/native",
+            let items = urlComponents.queryItems,
+            let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        let newProgressValue = Float(newValue)
+        view?.setProgressValue(newProgressValue)
+        
+        let shouldHideProgress = shouldHideProgress(for: newProgressValue)
+        view?.setProgressHidden(shouldHideProgress)
+    }
+    
+    func shouldHideProgress(for value: Float) -> Bool {
+        abs(value - 1.0) <= 0.0001
+    }
+    
+    func viewDidLoad() {
+        var urlComponents = URLComponents(string: unsplashAuthorizeURLString)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: accessKey),
+            URLQueryItem(name: "redirect_uri", value: redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: accessScope)
+        ]
+        let url = urlComponents.url!
+        
+        didUpdateProgressValue(0)
+        
+        let request = URLRequest(url: url)
+        view?.load(request: request)
+    }
+    
+    weak var view: WebViewViewControllerProtocol?
+}
